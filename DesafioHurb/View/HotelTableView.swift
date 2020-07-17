6cb8c27b33fde28e72681f3f482bea179d4d0d72
Gabriel Coutinho <gcoutinhorj@gmail.com>
2020-07-16 21:13:53 -0300

    adicionando gitignore

---
+++ DesafioHurb/View/HotelTableView.swift
@@ -0,0 +1,21 @@
+//
+//  HotelTableView.swift
+//  DesafioHurb
+//
+//  Created by Gabriel Coutinho on 16/07/20.
+//  Copyright © 2020 Gabriel Coutinho. All rights reserved.
+//
+
+import UIKit
+
+class HotelTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
